de0fa3f892cd8494f7ad9952223ff389c2d637c5
lars <lars.scheme@gmail.com>
2022-04-22 11:42:43 +0200

    Add doc comments to the lib

--- bin2delphi.go
+++ bin2delphi.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 )
 
+// NewUnit creates a new empty unit of the given name. You can call AddConstant
+// on the Unit to fill it with constants. Call Generate when you are done to get
+// the code.
 func NewUnit(name string) *Unit {
 	u := &Unit{isUTF8: isUTF8(name)}
 	fmt.Fprintf(u, "unit %s;\r\n\r\ninterface", name)
 	return u
 }
 
+// Unit represents a Delphi file (.pas). Create one with NewUnit, add constants
+// with AddConstant and then generate the code with Generate.
 type Unit struct {
 	bytes.Buffer
 	isUTF8 bool
@@ -25,6 +30,8 @@ func isUTF8(s string) bool {
 	return false
 }
 
+// AddConstant adds a byte array constant of the given name, with the given data
+// to the unit.
 func (u *Unit) AddConstant(name string, data []byte) {
 	u.isUTF8 = u.isUTF8 || isUTF8(name)
 
@@ -58,6 +65,11 @@ func (u *Unit) AddConstant(name string, data []byte) {
 	}
 }
 
+// Generate returns the Delphi code for the unit. It contains the unit name, all
+// the constants and an empty implementation section.
+//
+// You can only call Generate once. If you call it multiple times, the generated
+// code will be broken.
 func (u *Unit) Generate() []byte {
 	u.WriteString("\r\n\r\nimplementation\r\n\r\nend.\r\n")
 	if u.isUTF8 {
